731a35ed3d3a90a5959a57d813773a079e199400
ViCor44 <victor.a.correia@gmail.com>
2022-09-04 21:09:09 +0100

    partials

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'recipes/home.html', context={
+    return render(request,'recipes/pages/home.html', context={
         'name': 'Victor Correia',
     })
     
